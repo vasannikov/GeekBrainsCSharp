b61e0cb6594d9999fcfa2910c66d1e0b78a8a01a
vasannikov <vasannikov@edu.hse.ru>
2023-09-26 17:10:54 +0300

    solved seminar 4

--- seminar4task27/Program.cs
+++ seminar4task27/Program.cs
@@ -1,10 +1,15 @@
-﻿Console.WriteLine ("Введите число:");
-int number = int.Parse(Console.ReadLine()??"0");
-int[] digits = new int[(int)Math.Log10(number) + 1]; 
-int result = 0;
-for (int i = 0; i < digits.Length; i++, number /= 10){
+﻿int Sum (int number, int[] digits){
+    int result = 0;
+    for (int i = 0; i < digits.Length; i++, number /= 10){
     digits[i] = number % 10;
     result += digits[i];
+}  
+    return result;
 }
-Console.WriteLine("Результат работы приложения:");
-Console.WriteLine(result);
+
+    Console.WriteLine ("Введите число:");
+    int number = int.Parse(Console.ReadLine()??"0");
+    int[] digits = new int[(int)Math.Log10(number) + 1]; 
+    int answer = Sum(number, digits);
+    Console.WriteLine("Результат работы приложения:");
+    Console.WriteLine(answer);
